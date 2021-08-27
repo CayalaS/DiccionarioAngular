15f3b0341df093c65ef712af3ed6d8b67872fdbb
Carlos Ayala <carlos.ayala@bosonit.com>
2021-08-27 13:32:51 +0200

    Primer Commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+const MaterialComponents = [
+  MatInputModule
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
